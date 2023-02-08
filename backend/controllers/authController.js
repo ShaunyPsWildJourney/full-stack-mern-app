d6e77ca808d2b8ae3824289a23f28fea3821a03e
Shaun Pickett <98673276+MusicShaun@users.noreply.github.com>
2023-02-09 07:31:59 +0800

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -27,7 +27,7 @@ exports.signup = ash(async (req, res) => {
       status: 'success',
       firstName: newUser.firstName,
       lastName: newUser.lastName,
-      id: newUser._id,
+      _id: newUser._id,
       email: newUser.email,
       token
     })
@@ -48,7 +48,7 @@ exports.login = ash(async (req, res, next) => {
     status: 'success',
     firstName: user.firstName,
     lastName: user.lastName,
-    id: user._id,
+    _id: user._id,
     email: user.email,
     token,
     profilePicture: user.profilePicture
